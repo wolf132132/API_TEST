0cf85916d0d49105cdf85771870bc3fde0756b09
Zirong Wang <wolf132132@gmail.com>
2018-08-27 22:36:12 -0700

    add update method and connect it with database. Now item can be updated.

--- code/item.py
+++ code/item.py
@@ -6,17 +6,6 @@ class Item(Resource):
 	parser = reqparse.RequestParser()
 	parser.add_argument('price', type=float, required=True, help='This field cannot be left blank')
 
-	"""
-	next will return the first object it finds
-	if it finds nothing, it will return None by putting None as a parameter
-	"""
-	@jwt_required()
-	def get(self, name):
-		item = self.find_by_name(name)
-		if item:
-			return item
-		return {'message': 'item not found'}, 404
-
 	@classmethod
 	def find_by_name(cls, name):
 		connection = sqlite3.connect('database.db')
@@ -28,6 +17,36 @@ class Item(Resource):
 		if row:
 			return {'items': {'name': row[0], 'price': row[1]}}
 
+	@classmethod
+	def insert(cls, item):
+		connection = sqlite3.connect('database.db')
+		cursor = connection.cursor()
+		query = "INSERT INTO items VALUES (?, ?) "
+		cursor.execute(query, (item['name'], item['price']))
+		connection.commit()
+		connection.close()
+
+	@classmethod
+	def update(cls, item):
+		connection = sqlite3.connect('database.db')
+		cursor = connection.cursor()
+		query = "UPDATE items SET price=? WHERE name=?"
+		cursor.execute(query, (item['price'], item['name']))
+		connection.commit()
+		connection.close()
+
+
+	"""
+	next will return the first object it finds
+	if it finds nothing, it will return None by putting None as a parameter
+	"""
+	@jwt_required()
+	def get(self, name):
+		item = self.find_by_name(name)
+		if item:
+			return item
+		return {'message': 'item not found'}, 404
+
 	def post(self, name):
 		if self.find_by_name(name):
 			return {'message': 'Item {} has existed already'.format(name)}, 400
@@ -35,12 +54,10 @@ class Item(Resource):
 		# create json payload
 		# force=True means no content-header type required
 		item = {'name': name, 'price': data['price']}
-		connection = sqlite3.connect('database.db')
-		cursor = connection.cursor()
-		query = "INSERT INTO items VALUES (?, ?) "
-		cursor.execute(query, (item['name'], item['price']))
-		connection.commit()
-		connection.close()
+		try:
+			self.insert(item)
+		except:
+			return {'message': 'An internal error occurs'}, 500
 		return item, 201
 
 	def delete(self, name):
@@ -55,12 +72,14 @@ class Item(Resource):
 
 	def put(self, name):
 		data = Item.parser.parse_args()
-		item = next(filter(lambda x: x['name'] == name, items), None)
+		item = self.find_by_name(name)
+		updated_item = {'name': name, 'price': data['price']}
+
 		if item is None:
-			item = {'name': name, 'price': data['price']}
+			self.insert(updated_item)
 		else:
-			item.update(data)
-		return item
+			self.update(updated_item)
+		return updated_item
 
 
 class ItemList(Resource):
