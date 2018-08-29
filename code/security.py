d73a270ea2cc8e0ef65237d76a3ff52ece818b8e
Zirong Wang <wolf132132@gmail.com>
2018-08-28 20:19:43 -0700

    add models

--- code/security.py
+++ code/security.py
@@ -1,4 +1,4 @@
-from user import User
+from models.user_model import UserModel
 from werkzeug.security import safe_str_cmp
 
 """
@@ -20,12 +20,13 @@ user_id_mapping = {
 }
 """
 
+
 def authenticate(username, password):
-    user = User.find_by_username(username)
+    user = UserModel.find_by_username(username)
     if user and safe_str_cmp(user.password, password):
         return user
 
 
 def identity(payload):
     user_id = payload['identity']
-    return User.find_by_id(user_id)
+    return UserModel.find_by_id(user_id)
